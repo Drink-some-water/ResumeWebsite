dc81fcf509d21acd54c73d2622596842f719eda2
Sean Collins <Sean.Collins01@student.csulb.edu>
2024-06-05 15:34:39 -0700

    added jank resume button

--- resume-website/src/App.js
+++ resume-website/src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 import WebGLScene from './WebGLScene1';
 
 function App() {
+  const openResume = () => {
+    window.open(`${process.env.PUBLIC_URL}/Sean Collins Resume 2024 data-4.pdf`, '_blank');
+  };
+
   return (
     <div className="App">
       <header className="App-header"> Hello World
       </header>
-      <WebGLScene/>
+      <button onClick={openResume}>Resume</button>
+      <WebGLScene />
     </div>
   );
 }
